c762619ccfb6e449d3be667f03f99d2045bd113e
worawut <worawut48@hotmail.co.th>
2023-10-18 10:24:10 +0700

    add login

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.all('/api/scholarship/login', (req, res) => {
+    console.log("Just got a request!")
+    res.send({result : {accessToken : 'hello accessToken'}})
+})
 app.listen(process.env.PORT || 3000)
